f121871eac4a9ed0e1abe2fd540b0c763e731309
@maxvgr <support@ids.spb.ru>
2026-08-31 13:26:37 +0300

    feat: добавлено мобильное меню

--- src/js/layout/menu.js
+++ src/js/layout/menu.js
@@ -7,20 +7,27 @@ if (menu) {
   const button = header.querySelector('.hamburger');
 
   const toggleMenu = (isOpen) => {
-    menu.classList.toggle(StyleClass.state.open, isOpen);
+    menu.classList.toggle(StyleClass.mobile.open, isOpen);
     button.classList.toggle(StyleClass.state.active, isOpen);
     document.body.classList.toggle(StyleClass.body.scroll, isOpen);
+
+    menu.setAttribute('aria-hidden', String(!isOpen));
+    button.setAttribute('aria-expanded', String(isOpen));
   };
 
   window.addEventListener('click', (e) => {
     const target = e.target;
 
     if (target.closest('.hamburger')) {
-      const isMenuOpen = menu.classList.contains(StyleClass.state.open);
+      const isMenuOpen = menu.classList.contains(StyleClass.mobile.open);
       toggleMenu(!isMenuOpen);
+    } else if (!target.closest('.mobile-menu__content')) {
+      toggleMenu(false);
     }
+  });
 
-    else if (!target.closest('.mobile-menu__content')) {
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
       toggleMenu(false);
     }
   });
