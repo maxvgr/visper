f8b8dedd366d4777cda0a67194e39eef96a12087
@maxvgr <support@ids.spb.ru>
2026-09-02 12:14:06 +0300

    refactor: футер

--- src/js/global/init.js
+++ src/js/global/init.js
@@ -92,11 +92,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   window.App.gallery = new Gallery();
   window.App.tab = new Tab();
-  window.App.scrollTop = new ScrollTop({
-    selector: ".footer-bottom__right a",
-    footerMode: "off",
-    showThreshold: 0,
-  });
+  window.App.scrollTop = new ScrollTop();
 
   PlayVideoInViewport();
 });
